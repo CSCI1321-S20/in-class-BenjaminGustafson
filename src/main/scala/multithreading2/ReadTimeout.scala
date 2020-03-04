95b73127616b285d45b6ba53c09b9a9fd3971c61
BenjaminGustafson <bgustafs@trinity.edu>
2020-03-04 10:16:39 -0600

    more multithreading

--- src/main/scala/multithreading2/ReadTimeout.scala
+++ src/main/scala/multithreading2/ReadTimeout.scala
@@ -3,20 +3,31 @@ package multithreading2
 import io.StdIn._
 
 object ReadTimeout extends App {
+    var answered = false
+    var timeUp = false
     val countThread = new Thread{
         override def run: Unit = {
-            for(i <- 10 to 1 by -1){
+            var i = 10
+            while(!answered && i > 0){
                 println(i)
                 Thread.sleep(1000)
+                i -= 1
             }
-            println("Time's up.")
-            sys.exit(0)
+            if(!answered){
+                println("Time's up.")
+                timeUp = true
+            }
         }
     }
     println("Enter your age.")
     countThread.start()
-    val age = readInt
-    if(age < 18) println("Sorry, you can't enter.")
-    else println("Welcome.")
-    sys.exit(0)
+    while(!timeUp && !Console.in.ready){
+        Thread.sleep(10)
+    }
+    if(!timeUp){
+        val age = readInt
+        answered = true
+        if(age < 18) println("Sorry, you can't enter.")
+        else println("Welcome.")
+    }
 }
